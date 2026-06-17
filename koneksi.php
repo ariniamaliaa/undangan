1f08750b43fc7fea182a34370c99bb700e647b45
ariniamalia <arini.amalia3112@gmail.com>
2026-06-17 09:26:12 +0700

    rubah

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,15 @@
 <?php
+// Buka panel InfinityFree, masuk ke menu "MySQL Databases"
+// Masukkan data sesuai yang ada di sana, jangan gunakan localhost!
+$host = "sql213.infinityfree.com"; // Pastikan ini sesuai dengan "MySQL Hostname" di panel
+$user = "if0_42200847";            // "MySQL Username"
+$pass = "gv2qGXOjkz";  // Password database yang kamu set
+$db   = "if0_42200847_db_undangan"; // "Database Name"
 
-$conn = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "db_undangan"
-);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-if(!$conn){
-    die("Koneksi Gagal: " . mysqli_connect_error());
+// Cek apakah koneksi berhasil
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
